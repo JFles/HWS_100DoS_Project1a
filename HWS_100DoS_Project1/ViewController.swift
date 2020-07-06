68d4d4b5f1d32745a3decdb733913a514d0d6188
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-07-05 21:10:37 -0500

    Initial Commit

---
+++ HWS_100DoS_Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS_100DoS_Project1
+//
+//  Created by Jeremy Fleshman on 7/5/20.
+//  Copyright © 2020 Jeremy Fleshman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
