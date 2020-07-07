7f5628ab0f01ba5d4ae9e4362d4972979dda7cb9
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-07-07 11:43:03 -0500

    Completed Day 16. Simple day -- setting up project, adding pic resources to display, creating basic UITableView in storyboard and adding basic UITableView delegates to the VC.swift

--- HWS_100DoS_Project1/ViewController.swift
+++ HWS_100DoS_Project1/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    // MARK: - Properties
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // singleton for the shared file manager
+        let fileManager = FileManager.default
+
+        // path for current executables bundle's resources
+        let path = Bundle.main.resourcePath!
+
+        // shallow search (doesn't follow symb links or sub dirs)
+        // returns [String] of paths found
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
+
 
